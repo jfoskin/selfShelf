b97a5c7fb44c32da89240ec3694ebb958278331b
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-06-15 13:59:44 -0400

    created basic server and database connection

---
+++ server.js
@@ -0,0 +1,39 @@
+// Dependancies
+
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+
+const app = express()
+const PORT = process.env.PORT
+const uri = process.env.DATABASEURL
+
+//Database connection
+
+mongoose.connect(uri)
+
+const db = mongoose.connection
+
+db.on('error', (error)=> console.log(error.message,`Database has an error`))
+db.on('connected', ()=> console.log(`Database has successfully connected`))
+db.on('disconnected', ()=> console.log(`Database has disconnected`))
+
+
+
+//Middleware
+
+
+
+//Routes
+app.get('/', (req,res)=>{
+    res.status(200).send(`welcome to my library where I reflect on the books I have read and take suggestions `)
+})
+
+app.post('/book', (req, res)=>{
+    res.send(`books recieved`)
+})
+
+//Ports
+app.listen(PORT, ()=>{
+    console.log(`Lstening to server on http://localhost:${PORT}`)
+})
