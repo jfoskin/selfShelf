99972a24de4299ee82128e5debd937994076a650
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-06-15 17:28:46 -0400

    updated post route to create book

--- server.js
+++ server.js
@@ -16,32 +16,38 @@ mongoose.connect(uri)
 
 const db = mongoose.connection
 
-db.on('error', (error)=> console.log(error.message,`Database has an error`))
-db.on('connected', ()=> console.log(`Database has successfully connected`))
-db.on('disconnected', ()=> console.log(`Database has disconnected`))
+db.on('error', (error) => console.log(error.message, `Database has an error`))
+db.on('connected', () => console.log(`Database has successfully connected`))
+db.on('disconnected', () => console.log(`Database has disconnected`))
 
 
 //Middleware
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 
 //Routes
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.status(200).send(`welcome to my library where I reflect on the books I have read and take suggestions `)
 })
 
-app.post('/book', (req, res)=>{
+app.post('/book', (req, res) => {
 
-    if (req.body.completed === 'on'){
+    if (req.body.completed === 'on') {
         req.body.completed = true
-    }else{
+    } else {
         req.body.completed = false
     }
+
+    Book.create(req.body).then((createdBook) => {
+        console.log(`Book was created`)
+    }).catch(err => {
+        console.log(err, `<<<<<========= ERROR!!!!`)
+    })
 
     res.send(req.body)
 })
 
 //Ports
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Lstening to server on http://localhost:${PORT}`)
 })
