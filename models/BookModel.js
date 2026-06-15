3797f4041d936cf67664099287bc5b311e2c614a
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-06-15 17:19:24 -0400

    adjusted book model data

--- models/BookModel.js
+++ models/BookModel.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    author: {type: String, required: true},
-    completed: Boolean
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    completed: Boolean,
+    body: String,
+    comments: [{ body: String, date: Date }],
+    date: { type: Date, default: Date.now },
 });
 
 const Book = mongoose.model("Book", bookSchema);
