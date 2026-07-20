8183640fec3403f57c45cb6c4cd6292e1f86ceaa
Jazma Foskin <82051355+jfoskin@users.noreply.github.com>
2026-07-20 17:26:38 -0400

    feat: add role field to user schema for role management

--- models/UserModel.js
+++ models/UserModel.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true, minLength: 10 },
+    role: { type: String, enum: ['user', 'manager'], default: 'user' }
 })
 
 userSchema.pre('save', async function () {
